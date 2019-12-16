23538df2179bab21969d1f3b7a633a28a0a01f86
rafaelwi <rafwiskailnicki@gmail.com>
2019-12-15 21:20:57 -0500

    Added rough message queue that will save messages every 10 seconds

--- sds.go
+++ sds.go
@@ -8,8 +8,10 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/carlescere/scheduler"
 )
 
 func init() {
@@ -17,10 +19,14 @@ func init() {
 	flag.Parse()
 }
 
+// Create the array/slice that will act as a queue and save messages
+var msgQueue = make([]string, 0)
 var token string
 var buffer = make([][]byte, 0)
+var nextSDSMsg = time.Now().Add(time.Second * 15)
 
 func main() {
+	fmt.Println("next write: ", nextSDSMsg)
 	// Check if a token has been provided
 	if token == "" {
 		fmt.Println("[ERR!] No token has been provided. Rerun with ./sds -t " +
@@ -46,6 +52,10 @@ func main() {
 		fmt.Println("[ERR!] Could not open Discord session : ", err)
 		return
 	}
+
+	// Set up the scheduled job to run every so often
+	job := writeMsgsToFile
+	scheduler.Every(10).Seconds().Run(job)
 
 	// Wait here until CTRL-C is recieved
 	fmt.Println("[INFO] SDS is now running. Press CTRL-C to exit.")
@@ -92,5 +102,33 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	} else {
 		// Otherwise send the message
 		s.ChannelMessageSend(m.ChannelID, m.Content)
+		msgQueue = append(msgQueue, m.Content)
+		fmt.Println("Current queue: ")
+		for _, msgs := range msgQueue {
+			fmt.Println("[" + msgs + "]")
+		}
 	}
+}
+
+func writeMsgsToFile() {
+	// Open file
+	f, err := os.OpenFile("msglog.txt", os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		fmt.Println("[ERR!] Could not open file!", err)
+		return
+	}
+
+	defer f.Close()
+
+	// Write to file
+	for _, msg := range msgQueue {
+		f.WriteString(msg + "\xffff")
+	}
+	fmt.Println("Wrote queue to file")
+
+	// Reset timer
+	nextSDSMsg = time.Now().Add(time.Second * 10)
+
+	// Clear queue
+	msgQueue = make([]string, 0)
 }
