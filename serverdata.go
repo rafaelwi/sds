be503df97256f62d84b7524ddc94830ac5148356
rafaelwi <rafwiskailnicki@gmail.com>
2019-12-19 21:57:42 -0500

    Created guildData obj

---
+++ serverdata.go
@@ -0,0 +1,12 @@
+/*
+	serverdata.go
+	github.com/rafaelwi
+*/
+
+package main
+
+type guildData struct {
+	sliceID     int
+	guildID     string
+	logMsgCount int // Since this defaults to 0, no need to set a default
+}
